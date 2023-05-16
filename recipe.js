ebbb3aead4b17b507cf7377a01c1d3f94d120f7a
RazorSh4rk <iamrazorshark@gmail.com>
2023-05-17 00:19:22 +0200

    update some js things

--- recipe.js
+++ recipe.js
@@ -1,8 +1,38 @@
-const { getRecipe } = require("./recipe_handler.js");
+const { Configuration, OpenAIApi } = require("openai");
 
 exports.handler = async (event) => {
   if (event.requestContext.http.method == "POST") {
     const ingredients = JSON.parse(event.body).ingredients;
-    return await getRecipe(ingredients);
+    
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+      });
+    
+      print("KEY " + configuration.apiKey)
+    
+      const prompt = `
+              Give me 3 recipes in a json list format, fitting this example: 
+              [{'name': 'food_name', ingredients:['a', 'b', 'c'], steps:['do this', 'do that']}] 
+              if the available ingredients are these, and i want to have to buy the absolute
+              minimum extra: ${ingredients.join(", ")}
+          `;
+    
+      const openai = new OpenAIApi(configuration);
+      const res = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        max_tokens: 2048,
+        temperature: 0,
+      });
+    
+      const response = {
+        statusCode: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(res),
+      };
+    
+      return response;
   }
 };
